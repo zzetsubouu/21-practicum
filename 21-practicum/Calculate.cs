a493b8324857fa3ecb9569eb76193cd7f1aeb73a
zzetsubouu <pyanix.mladshiy@gmail.com>
2025-02-05 23:22:52 +0200

    1 задание

---
+++ 21-practicum/Calculate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_practicum
+{
+    public class Calculate
+    {
+        public double value1 { get; set; }
+        public double value2 { get; set; }
+        public Calculate()
+        {
+
+        }
+        public Calculate(double value1)
+        {
+            this.value1 = value1;
+        }
+        public Calculate(double value1, double value2) : this(value1)
+        {
+            this.value2 = value2;
+        }
+        public double CalculateSquareRoot()
+        {
+            return Math.Sqrt(value1 * value2);
+        }
+        public override string ToString()
+        {
+            return $"Информация об объекте: первое значение = {value1}, второе значение = {value2}";
+        }
+    }
+}
--- 21-practicum/Program.cs
+++ 21-practicum/Program.cs
@@ -10,6 +10,30 @@ namespace _21_practicum
     {
         static void Main(string[] args)
         {
+            // Создание объекта без параметров
+            Calculate calc1 = new Calculate();
+            calc1.value1 = 3.21;
+            calc1.value2 = 2.21;
+
+            // Создание объекта с явной инициализацией
+            Calculate calc2 = new Calculate(8.21, 4.21);
+
+            // Ввод значений с клавиатуры
+            Console.WriteLine("введите первое значение:");
+            double value1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите второе значение:");
+            double value2 = Convert.ToDouble(Console.ReadLine());
+            var userCalc = new Calculate(value1, value2);
+
+            // Вывод информации о каждом объекте
+            Console.WriteLine(calc1.ToString());
+            Console.WriteLine($"Корень квадратный из произведения чисел (calc1): {calc1.CalculateSquareRoot()}");
+
+            Console.WriteLine(calc2.ToString());
+            Console.WriteLine($"Корень квадратный из произведения чисел (calc2): {calc2.CalculateSquareRoot()}");
+
+            Console.WriteLine(userCalc.ToString());
+            Console.WriteLine($"Корень квадратный из произведения чисел (userCalc): {userCalc.CalculateSquareRoot()}");
         }
     }
 }
