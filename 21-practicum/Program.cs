11231022244834948318c98ea14ec8547374ad51
zzetsubouu <pyanix.mladshiy@gmail.com>
2025-02-05 23:34:24 +0200

    2 задание

--- 21-practicum/Program.cs
+++ 21-practicum/Program.cs
@@ -10,6 +10,33 @@ namespace _21_practicum
     {
         static void Main(string[] args)
         {
+            // создание объекта без параметров
+            Time time1 = new Time();
+            time1.hours = 5;
+            time1.minutes = 30;
+
+            // создание объекта с явной инициализацией
+            Time time2 = new Time(7, 45);
+
+            // ввод значений с клавиатуры для объекта с тремя параметрами
+            Console.WriteLine("Введите количество часов:");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество минут:");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество секунд:");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            var userTime = new Time(hours, minutes, seconds);
+
+            // Вывод информации о каждом объекте
+            Console.WriteLine(time1.ToString());
+            Console.WriteLine($"Общее количество минут (time1): {time1.TotalMinutes()}");
+
+            Console.WriteLine(time2.ToString());
+            Console.WriteLine($"Общее количество минут (time2): {time2.TotalMinutes()}");
+
+            Console.WriteLine(userTime.ToString());
+            Console.WriteLine($"Общее количество секунд (userTime): {userTime.TotalSeconds()}");
         }
     }
+
 }
---
+++ 21-practicum/Time.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_practicum
+{
+    public class Time
+    {
+        public int hours { get; set; }
+        public int minutes { get; set; }
+        public int seconds { get; set; }
+
+        // конструктор без параметров
+        public Time() { }
+
+        // конструктор с двумя параметрами(часы и минуты)
+        public Time(int hours, int minutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+
+        // конструктор с тремя параметрами(часы, минуты,секунды)
+        public Time(int hours, int minutes, int seconds) : this(hours, minutes)
+        {
+            this.seconds = seconds;
+        }
+
+        // метод для вычисления общего кол-ва минут
+        public int TotalMinutes()
+        {
+            return (hours * 60) + minutes;
+        }
+
+        // Метод для вычисления общего кол-ва секунд
+        public int TotalSeconds()
+        {
+            return (hours * 3600) + (minutes * 60) + seconds;
+        }
+
+        // Переопределение метода ToString
+        public override string ToString()
+        {
+            return $"Время: {hours} часов, {minutes} минут, {seconds} секунд";
+        }
+    }
+}
